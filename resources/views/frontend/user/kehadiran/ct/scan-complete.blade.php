6bb61725008228ef0521f887f17f3fce0c99b576
hannan <nan_s96@yahoo.com>
2020-08-11 00:08:17 +0800

    .

--- resources/views/frontend/user/kehadiran/ct/scan-complete.blade.php
+++ resources/views/frontend/user/kehadiran/ct/scan-complete.blade.php
@@ -20,15 +20,14 @@
                         <p>Nama Pelajar : <b>AMAR MAKRUF</b></p>
                         <p>No. K/P : <b>111111-11-111</b></p>
                         <p>Kelas : <b>4 SAINS 2</b></p>
-                        <h3>Temperature</h3>
-                        <h3>Temperature</h3>
+                        <h3>Suhu</h3>
 
                             <div class="">
                                 <div class="form-group row">
                                     <div class="col-md-4 offset-md-4">
                                         <h1 class="font-weight-bold" id="temperatue">35.7 °C</h1>
 
-                                        <p>Condition:</p>
+                                        <p>Keadaan:</p>
                                         <h4 id="condition"></h4>
                                     </div>
                                 </div>
